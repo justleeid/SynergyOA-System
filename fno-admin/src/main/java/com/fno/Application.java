f7a753fe27d4f1c826b2c459bfad35aa037f4b2f
lan <1527715476@qq.com>
2025-01-05 00:14:16 +0800

    优化application写法

--- fno-admin/src/main/java/com/fno/Application.java
+++ fno-admin/src/main/java/com/fno/Application.java
@@ -1,39 +1,42 @@
 package com.fno;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
+import org.slf4j.Logger;
+import org.springframework.context.event.EventListener;
 
 /**
  * 启动程序
- * 
- * @author ry
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
         org.flowable.spring.boot.FlowableSecurityAutoConfiguration.class})
-@ComponentScan(value={"com.fno.*"})
-public class Application
-{
-    public static void main(String[] args)
-    {
-        long startTime = System.currentTimeMillis();
-        // System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(Application.class, args);
-        long endTime = System.currentTimeMillis();
-        System.out.println("项目启动花费时间：" + (endTime - startTime)/1000 + "秒");
-        System.out.println("             ___                            ___                                                                                    \n" +
-                "            (   )                          (   )                                                                                   \n" +
-                "    .--.     | |_       .---.   ___ .-.     | |_           .--.     ___  ___    .--.      .--.      .--.       .--.        .--.    \n" +
-                "  /  _  \\   (   __)    / .-, \\ (   )   \\   (   __)       /  _  \\   (   )(   )  /    \\    /    \\    /    \\    /  _  \\     /  _  \\   \n" +
-                " . .' `. ;   | |      (__) ; |  | ' .-. ;   | |         . .' `. ;   | |  | |  |  .-. ;  |  .-. ;  |  .-. ;  . .' `. ;   . .' `. ;  \n" +
-                " | '   | |   | | ___    .'`  |  |  / (___)  | | ___     | '   | |   | |  | |  |  |(___) |  |(___) |  | | |  | '   | |   | '   | |  \n" +
-                " _\\_`.(___)  | |(   )  / .'| |  | |         | |(   )    _\\_`.(___)  | |  | |  |  |      |  |      |  |/  |  _\\_`.(___)  _\\_`.(___) \n" +
-                "(   ). '.    | | | |  | /  | |  | |         | | | |    (   ). '.    | |  | |  |  | ___  |  | ___  |  ' _.' (   ). '.   (   ). '.   \n" +
-                " | |  `\\ |   | ' | |  ; |  ; |  | |         | ' | |     | |  `\\ |   | |  ; '  |  '(   ) |  '(   ) |  .'.-.  | |  `\\ |   | |  `\\ |  \n" +
-                " ; '._,' '   ' `-' ;  ' `-'  |  | |         ' `-' ;     ; '._,' '   ' `-'  /  '  `-' |  '  `-' |  '  `-' /  ; '._,' '   ; '._,' '  \n" +
-                "  '.___.'     `.__.   `.__.'_. (___)         `.__.       '.___.'     '.__.'    `.__,'    `.__,'    `.__.'    '.___.'     '.___.'   \n");
+@ComponentScan(value = {"com.fno.*"})
+public class Application implements CommandLineRunner {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    @Value("${server.port}")
+    private int serverPort;
+    private static long startTime;
 
-        System.out.println("启动成功，欢迎使用");
+    public static void main(String[] args) {
+        startTime = System.currentTimeMillis();
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("--------------------启动成功------------------------");
+        LOGGER.info("--------------------访问端口{}---------------------", serverPort);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        long endTime = System.currentTimeMillis();
+        LOGGER.info("项目启动花费时间：{}秒", (endTime - startTime) / 1000);
     }
 }
